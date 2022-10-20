6a242431e0587744e3dce21a807d66a11e6a1751
GaspardKerner <66307006+GaspardKerner@users.noreply.github.com>
2022-10-20 14:50:29 +0200

    Update PRS_OVER_TIME_AUTOIMMUNE_IBD_CD_BY_VAR.R

--- PRS/PRS_OVER_TIME_AUTOIMMUNE_IBD_CD_BY_VAR.R
+++ PRS/PRS_OVER_TIME_AUTOIMMUNE_IBD_CD_BY_VAR.R
@@ -1,5 +1,5 @@
 ### WORK AT THE VARIANT LEVEL TO GET VARIANTS SIGNIFICANTLY ASSOCIATED WITH THE AGE OF THE SAMPLES BY THEMSELVES
-source("/Volumes/IGSR/GASPARD/DATING_SELECTION/ZEUS/RSCRIPTS_PAPIER/Building_PRS_autoimmune_infection.R")
+source("~/PRS/Building_PRS_autoimmune_infection.R")
 
 fenos=c("cd_build37_40266_20161107_bonferroni.txt",
         "ibd_build37_59957_20161107_bonferroni.txt")
